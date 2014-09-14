2a2a4ed7eb7729b58f3c19f7c48698ed9982b07d
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2014-09-15 02:58:48 +0300

    simple reader for feature flags
    maps by element name to field name
    sets readable name and value

---
+++ FeatureFlags.Abstractions.Test/When_reading_featureflags.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FeatureFlags.Abstractions.Test
+{
+    [TestFixture]
+    public class When_reading_featureflags
+    {
+        [Test]
+        public void should_read_value_for_feature_flag()
+        {
+            var features = FeatureFlags.Map<BaklavaAppFeatures>();
+
+            features.IceCream.IsOn().Should().Be(true);
+            features.IceCream.Name().Should().Be("ice-cream on baklava");
+        }
+    }
+
+    public class BaklavaAppFeatures
+    {
+        public Feature IceCream { get; internal set; }
+    }
+}
--- FeatureFlags.Abstractions/FeatureFlags.cs
+++ FeatureFlags.Abstractions/FeatureFlags.cs
@@ -1,6 +1,52 @@
-﻿namespace FeatureFlags.Abstractions
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace FeatureFlags.Abstractions
 {
     public class FeatureFlags
     {
+        public static T Map<T>() where T : new()
+        {
+            var togglesFile =
+                XDocument.Load(AppDomain.CurrentDomain.BaseDirectory + "/featureToggles.xml").Element("featureToggles") ??
+                new XElement("featureToggles");
+            var toggles = togglesFile.Elements().ToDictionary(e => e.Name.LocalName);
+            var features = new T();
+
+            toggles.Keys.ToList()
+                .ForEach(
+                    k =>
+                        features.GetType()
+                            .GetProperty(k, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase)
+                            .SetValue(features,
+                                new Feature(toggles[k].Attribute("name").Value,
+                                    toggles[k].Attribute("value").Value.Equals("on", StringComparison.OrdinalIgnoreCase))));
+
+            return features;
+        }
+    }
+
+    public class Feature
+    {
+        private readonly string _name;
+        private readonly bool _state;
+
+        public Feature(string name, bool state)
+        {
+            _name = name;
+            _state = state;
+        }
+
+        public bool IsOn()
+        {
+            return _state;
+        }
+
+        public string Name()
+        {
+            return _name;
+        }
     }
 }
